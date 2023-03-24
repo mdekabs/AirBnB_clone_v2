7307eacd8905029f5bc4d34ba119e4c072d9a4d7
Satguru cargo <unkellia002@gmail.com>
2023-03-24 12:53:20 +0200

    airbnb solutions

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- preparing Mysql server for this project
+-- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
